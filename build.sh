00966f2d715d00cbb8c465e3e125f2b36b6d485b
V <V@fbi.gov>
2024-01-25 10:09:41 +0000

    user var

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-apko build apko.yaml quay.io/nandithebull/pixi-base pixi-base.tar  &&\
+echo $DOCKER_USER
+apko build apko.yaml quay.io/$DOCKER_USER/pixi-base pixi-base.tar  &&\
     docker load < pixi-base.tar && \
-    docker tag quay.io/nandithebull/pixi-base:latest-amd64 quay.io/codegod100/pixi-base:latest && \
-    docker push quay.io/nandithebull/pixi-base:latest
+    docker tag quay.io/$DOCKER_USER/pixi-base:latest-amd64 quay.io/$DOCKER_USER/pixi-base:latest && \
+    docker push quay.io/$DOCKER_USER/pixi-base:latest
